e5341f6e2ad752b87eab30eb76f9d8744914bf67
bohdan <khomenkobohdanw@gmail.com>
2026-04-03 14:44:24 -0600

    start-express + cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,19 @@
-const str = "ddd sfas "
+import express from "express"
+import cors from "cors"
+const app = express();
+const PORT = 3000;
 
-console.log("test rel2" + str)
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("Hello Express 🚀")
+});
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from API" })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
