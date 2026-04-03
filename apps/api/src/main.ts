c0f7694a4b5ab1037cf0e7c5e7bc6c4e3d390bc9
bohdan <khomenkobohdanw@gmail.com>
2026-04-03 15:13:07 -0600

    prisma and dotenv

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,15 @@
 import express from "express"
 import cors from "cors"
-const app = express();
-const PORT = 3000;
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost"
+}))
 
 app.get("/", (req, res) => {
   res.send("Hello Express 🚀")
